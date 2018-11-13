b51c10de584678e9cfb7c834b8f240e2c80766d2
pekormos <43568724+pekormos@users.noreply.github.com>
2018-11-13 14:14:19 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager; 
 import java.sql.ResultSet; 
 import java.sql.Statement; 
+import java.sql.SQLException;
 
 
 public class InsultGenerator { 
