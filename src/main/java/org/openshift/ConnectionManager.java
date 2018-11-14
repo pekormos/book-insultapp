cccc815f0df612482553aae93785d0bf9d8218d0
pekormos <43568724+pekormos@users.noreply.github.com>
2018-11-14 16:30:12 +0100

    Update ConnectionManager.java

--- src/main/java/org/openshift/ConnectionManager.java
+++ src/main/java/org/openshift/ConnectionManager.java
@@ -1,12 +1,5 @@
-/**
- *
- */
 
 package oracleconnection;
-
-//import com.jcraft.jsch.JSch;
-//import com.jcraft.jsch.JSchException;
-//import com.jcraft.jsch.Session;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,8 +7,8 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 //databaseDriverType, databaseIP, databasePort, databaseSID, databaseUser, databaseUser
-    public static Connection getOracleConnection(String databaseDriverType, String databaseIP, int databasePort, String databaseSID, String databaseUser, String databasePassword) throws SQLException {
-        return DriverManager.getConnection("jdbc:oracle"+ ":" + databaseDriverType + ":" + "@" + databaseIP + databasePort + ":" + databaseSID, databaseUser, databasePassword);
+    public static Connection getOracleConnection(String iDatabaseDriverType, String iDatabaseIP, int iDatabasePort, String iDatabaseSID, String iDatabaseUser, String iDatabasePassword) throws SQLException {
+        return DriverManager.getConnection("jdbc:oracle"+ ":" + iDatabaseDriverType + ":" + "@" + iDatabaseIP + iDatabasePort + ":" + iDatabaseSID, iDatabaseUser, iDatabasePassword);
     }
 
 
