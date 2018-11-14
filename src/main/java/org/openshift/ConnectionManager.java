11cc4aa13f9691a6772e0bc50e6af1730375e7f6
pekormos <43568724+pekormos@users.noreply.github.com>
2018-11-14 16:32:44 +0100

    Update ConnectionManager.java

--- src/main/java/org/openshift/ConnectionManager.java
+++ src/main/java/org/openshift/ConnectionManager.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 //databaseDriverType, databaseIP, databasePort, databaseSID, databaseUser, databaseUser
-    public static Connection getOracleConnection(String iDatabaseDriverType, String iDatabaseIP, int iDatabasePort, String iDatabaseSID, String iDatabaseUser, String iDatabasePassword) throws SQLException {
+    public static Connection getOracleConnection(String iDatabaseDriverType, String iDatabaseIP, String iDatabasePort, String iDatabaseSID, String iDatabaseUser, String iDatabasePassword) throws SQLException {
         return DriverManager.getConnection("jdbc:oracle"+ ":" + iDatabaseDriverType + ":" + "@" + iDatabaseIP + iDatabasePort + ":" + iDatabaseSID, iDatabaseUser, iDatabasePassword);
     }
 
 
-    public static Connection getOracleConnection(String databaseDriverType, String databaseIP, int databasePort, String databaseSID, String databaseUser, String databasePassword){
+    public static Connection getOracleConnection(String databaseDriverType, String databaseIP, String databasePort, String databaseSID, String databaseUser, String databasePassword){
         // TODO Auto-generated method stub
         return null;
     }
