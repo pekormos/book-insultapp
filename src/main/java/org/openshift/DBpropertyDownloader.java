784c496eeeeec9572e3c9fe8c4747613fc7a0cf8
pekormos <43568724+pekormos@users.noreply.github.com>
2018-11-14 16:37:32 +0100

    Update DBpropertyDownloader.java

--- src/main/java/org/openshift/DBpropertyDownloader.java
+++ src/main/java/org/openshift/DBpropertyDownloader.java
@@ -1,13 +1,14 @@
 package oracleconnection;
 
 public class DBpropertyDownloader {
+    
 
- public static void getDbProperty(Object oracleDbParameters) {
-        oracleDbParameters.databaseUser = System.getenv("ORACLE_ELINK_USER");
-        oracleDbParameters.databasePassword = System.getenv("ORACLE_ELINK_PASSWORD");
-        oracleDbParameters.databaseIP =  System.getenv("ORACLE_SERVICE_IP");
-        oracleDbParameters.databasePort = System.getenv("ORACLE_SERVICE_PORT");
-        oracleDbParameters.databaseSID = System.getenv("ORACLE_DATABASE_SERVICE_ID");
+ public static void getDbProperty(DbParams iOracleDbParameters) {
+        iOracleDbParameters.databaseUser = System.getenv("ORACLE_ELINK_USER");
+        iOracleDbParameters.databasePassword = System.getenv("ORACLE_ELINK_PASSWORD");
+        iOracleDbParameters.databaseIP =  System.getenv("ORACLE_SERVICE_IP");
+        iOracleDbParameters.databasePort = System.getenv("ORACLE_SERVICE_PORT");
+        iOracleDbParameters.databaseSID = System.getenv("ORACLE_DATABASE_SERVICE_ID");
     }
 
 }
