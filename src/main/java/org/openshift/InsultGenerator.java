d8f159ecff7235785d79db2cee27171609f2097f
pekormos <43568724+pekormos@users.noreply.github.com>
2018-11-15 11:57:13 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -9,18 +9,20 @@ import java.sql.SQLException;
 
 public class InsultGenerator { 
 	public String generateInsult() { 
- 	 	 String databaseUser = System.getenv("ORACLE_ELINK_USER");
+   
+       String databaseUser = System.getenv("ORACLE_ELINK_USER");
        String databasePassword = System.getenv("ORACLE_ELINK_PASSWORD");
        String databaseIP =  System.getenv("ORACLE_SERVICE_IP");
        String databasePort = System.getenv("ORACLE_SERVICE_PORT");
        String databaseSID = System.getenv("ORACLE_DATABASE_SERVICE_ID");
+       String returnstring;
      try {
 			//	USE DRIVER
-    
+			          
             try {
                 //The forName() method of Class class is used to register the driver class. This method is used to dynamically load the driver class.
                 Class.forName("oracle.jdbc.driver.OracleDriver");
-            } catch (Exception e) {
+            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
                 System.out.println("Where is your Oracle JDBC Driver?");
                 return "Where is your Oracle JDBC Driver?";
@@ -38,20 +40,21 @@ public class InsultGenerator {
 			Statement stmt=con.createStatement();
 
 			//	EXECUTE QUERY
-			ResultSet rs=stmt.executeQuery("select * from SLA_PENALTY");		//	FOR LONGER QUERIES {}
+			ResultSet rs=stmt.executeQuery("select * from SLA_PENALTY");	
 			while(rs.next()) {
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+                returnstring += rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3);
             }
 			//	CLOSE DB CONNECTION
 			rs.close();
             con.close();
+            return returnstring;
             }
             else{
                return  "Connection is null!";
             }
           }	catch(Exception e){ 
                 System.out.println(e);
-                return "Failure in try";
+                return con + ", Failure in try" + ", connection string is:" + "jdbc:oracle:thin:@10.105.68.72:1521:"+databaseSID,databaseUser,databasePassword;
             }
 	return "Cannot reach this point";
  	} //public String generateInsult()
